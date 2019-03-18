6577f264aab66444b43ee6fd748402e682ba5dde
dothinking <wuyunjian2007@163.com>
2019-03-19 00:50:45 +0800

    update testing data

--- test/test_data.py
+++ test/test_data.py
@@ -17,24 +17,24 @@ def fun_groups(res, level=4):
 	global num	
 	for i in range(level):
 		num += 1
-		x = ["groups_{0}".format(num), num, []]
+		x = ["Groups_{0}".format(num), num, []]
 		res.append(x)
 		fun_groups(x[2], level-1)
 
 def fun_tags(num):
-	return [[i, "Tag_{0}".format(i), randomColor()] for i in range(num)] # tag=0 => default tag
+	return [[i, "Tag_{0}".format(i+1), randomColor()] for i in range(num)]
 
 
 def fun_items(max_item, max_group, max_tag):
-	scriptPath = os.path.abspath(__file__)
+	scriptPath = os.path.dirname(os.path.abspath(__file__))
 	c_time = time.strftime("%Y-%m-%d",time.localtime(time.time()))
 	return [[
-		"name_bla_bla_{0}".format(i), 
+		"name_bla_bla_{0}".format(i+1), 
 		random.randint(1,max_group), 
-		list(set([random.randint(1,max_tag) for i in range(random.randint(1,max_tag))])), 
+		list(set([random.randint(1,max_tag) for k in range(random.randint(1,max_tag))])), 
 		scriptPath, 
 		c_time, 
-		"blablabla" if random.randint(1,10)>5 else ""] for i in range(1, max_item+1)]
+		"blablabla" if random.randint(1,10)>5 else "hiahiahia"] for i in range(max_item)]
 
 
 
@@ -45,11 +45,14 @@ if __name__ == "__main__":
 
 	groups=[['Ungrouped', 1, []], ['Unreferenced', 2, []], ['All Groups', 3, []]] # default groups
 	fun_groups(groups, group_level)
+
+	tags = [[0, 'NoTag', '#000000']] # default tag
+	tags.extend(fun_tags(num_tag))	
 	
 	data = {
 		"Tagit": "0.5", 
 		"groups": ["Group", "key", groups],
-		"tags": fun_tags(num_tag),
+		"tags": tags,
 		"items": fun_items(num_item, num, num_tag)
 	}
 
