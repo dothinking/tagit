cc26746d3a066a4cbede6944b8e977214ee7abe4
dothinking <train8808@gmail.com>
2019-03-28 22:49:15 +0800

    update path in bat

--- tagit.bat
+++ tagit.bat
@@ -1,8 +1,7 @@
 @echo off 
 
 if "%1" == "h" goto label
-mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit 
+mshta vbscript:createobject("wscript.shell").run("""%~dp0%~nx0"" h",0)(window.close)&&exit 
 
 :label
-
-python ./Tagit.py
+python %~dp0/Tagit.py
